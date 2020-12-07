4f30a11f56edef5f217e257738d04b1ba7b80515
Carlos Jimenez <cjimenez02@gmail.com>
2020-12-07 11:54:19 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KlarnaMobileSDK",
-            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.0.29/KlarnaMobileSDK.xcframework.zip",
-            checksum: "3fc09afec15e984f3645e7fc95d97d896ced8c3a60b207845178e16c1e6ff1dc"
+            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.0.30/KlarnaMobileSDK.xcframework.zip",
+            checksum: "9cb751127b4072f37aab1af73900d0ea41b9b4e579c6526fc5324becd4110c6f"
         )
     ]
 )
