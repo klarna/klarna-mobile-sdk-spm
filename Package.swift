3b48c8a8614a219df0305f76ef932561e0974a86
Mobile-sdk <mobile.sdk@klarna.com>
2021-05-06 17:55:57 +0200

    update package v2.0.41

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KlarnaMobileSDK",
-            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.0.40/KlarnaMobileSDK-full.xcframework.zip",
-            checksum: "17829629d9034ef45c2bad7a4010e160c58c8dda10b79db78b92afcefaacde79"
+            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.0.41/KlarnaMobileSDK-full.xcframework.zip",
+            checksum: "77a697c28d1d08ee369dd4216fdcfce805a3400f37ae7befc02d83ee775b58c3"
         )
     ]
 )
