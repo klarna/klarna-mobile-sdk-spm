6c7db33551913877d559358aa4bedf81f74d3fd5
Carlos Jimenez <carlos.jimenez@klarna.com>
2021-08-04 11:48:36 +0200

    Rename binary target.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             checksum: "2f099ae5b059cab65281504f2e7d29a4acd43d4d497d61f83277dc52727ec9f3"
         ),
         .binaryTarget(
-            name: "KlarnaMobileSDK-basic",
+            name: "KlarnaMobileSDK",
             url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.0.43/KlarnaMobileSDK-basic.xcframework.zip",
             checksum: "9b63a1e41b35936a7a382e8e57d64c9da460e34dd2297f1059d1c65f87e9be4e"
         ),
