885912d0bf6be2c5029683ad68de963585755590
Klarna Mobile SDK <67329369+klarna-msdk@users.noreply.github.com>
2024-10-23 07:25:14 +0000

    update package v2.6.23

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KlarnaMobileSDK",
-            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.6.22/KlarnaMobileSDK-basic.xcframework.zip",
-            checksum: "4118bcd9f63f5edfb933d929b1be36357b6d222a95c67e5402bdc5f5b7d98972"
+            url: "https://github.com/klarna/klarna-mobile-sdk/releases/download/2.6.23/KlarnaMobileSDK-basic.xcframework.zip",
+            checksum: "8c63d32cdab981b66beb8241fc09eeec57ee8eb6c73911b9ade3f8e1b2410d9c"
         )
     ]
 )
